e851bcd89b349b34f2d287d466b8d2c92783091c
Samuel Eagan <samueleagan@gmail.com>
2020-11-30 04:12:49 -0600

    Add text to a button? maybe?

--- ToDoListApp/ToDoListApp/FirstViewController.swift
+++ ToDoListApp/ToDoListApp/FirstViewController.swift
@@ -8,18 +8,36 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UITextFieldDelegate {
     
     @IBOutlet var tableView: UITableView!
+    @IBOutlet var textField: UITextField!
     
     var tasks = [Task]()
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        textField.delegate = self
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func tapAddButton(_ sender: UIButton) {
+        if let textItem = textField.text, !textItem.isEmpty {
+            tasks.append(Task(taskText: textItem))
 
+            tableView.beginUpdates()
+            tableView.insertRows(at: [IndexPath.init(row: tasks.count-1, section: 0)], with: .automatic)
+            tableView.endUpdates()
+        }
+        textField.text = nil
+        
+    }
+    
 
 }
 
@@ -38,9 +56,7 @@ extension FirstViewController: UITableViewDataSource {
     
     public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        
         cell.textLabel?.text = tasks[indexPath.row].taskText
-        
         return cell
     }
     
