d8dc5740137ba60c70719bea42a14c033902cd0f
sumana Reddy <sumanareddy.guc27@gmail.com>
2020-12-03 12:30:53 -0500

    added code to view controller

--- ToDoListApp/ToDoListApp/ViewController.swift
+++ ToDoListApp/ToDoListApp/ViewController.swift
@@ -11,10 +11,12 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet var tableView: UITableView!
+    
+    var tasks = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // get all current saved tasks
     }
 
 
@@ -29,6 +31,18 @@ extension ViewController: UITableViewDelegate {
 }
 
 extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        cell.textLabel?.text = tasks[indexPath.row]
+        
+        return cell
+    }
     
 }
 
